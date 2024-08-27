37363416f71b685511b2c914f0101799820cf4d7
KyleWinston <kyle.w.r@hotmail.com>
2024-08-27 19:57:46 -0400

    soundfont updates

--- src/soundfont/events.rs
+++ src/soundfont/events.rs
@@ -11,12 +11,13 @@ pub fn play_sf(
     mut sf_play: EventReader<SFplayEvent>,
     audio: Res<AudioChannel<SfChannel>>,
 ) {
+    audio.stop();
     for ev in sf_play.read() {
         let ev = &ev.0;
         audio
             .play(asset_server.load("audio/soundfonts/".to_owned() + &ev.path + ".wav"))
+            .with_volume(ev.volume)
             .start_from(ev.target_chunk[0])
-            .end_at(ev.target_chunk[1])
-            .with_volume(ev.volume);
+            .end_at(ev.target_chunk[1]);
     }
 }
