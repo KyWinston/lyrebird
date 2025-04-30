a11d201676fb04c4d6d585692a53c5b9812b62f7
ky roberts <kyle.w.r@hotmail.com>
2025-04-30 19:58:38 -0400

    migration changes

--- src/sfx/mod.rs
+++ src/sfx/mod.rs
@@ -1,7 +1,6 @@
 use bevy::prelude::*;
 use components::SfxEmitter;
-use events::PlayTone;
-use systems::play_sfx;
+use systems::{play_sfx, process_spatial_damping};
 
 pub mod components;
 pub mod resources;
@@ -11,6 +10,6 @@ pub struct SfxPlugin;
 impl Plugin for SfxPlugin {
     fn build(&self, app: &mut App) {
         app.register_type::<SfxEmitter>()
-            .add_systems(Update, (process_spatial_damping, process_spatial_damping));
+            .add_systems(Update, process_spatial_damping);
     }
 }
